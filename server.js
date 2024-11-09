7fd6fe12b4f73b807df01434f295264f04f1bac5
MariliaEdite <160150265+MariliaEdite@users.noreply.github.com>
2024-11-09 08:54:15 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+//Marília Edite
+//2A
+
+const express = require('express');
+const app = express();
+
+let objetos = [];
+
+app.get('/publicacao/', (req, res) => {
+    res.json(objetos);
+})
+
+
+app.post('/publicacao/:texto', (req, res) => {
+    let objetoTeste = {
+        texto: req.params.texto,
+    }
+    res.json(objetoTeste);
+    objetos.push(objetoTeste)
+});
+
+app.delete('/publicacao/', (req, res) => {
+    objetos.pop()
+    res.send("Deletado")
+})
+
+app.listen(3001, () => {
+    console.log('Server started');
+});
